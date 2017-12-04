f14a82a5bd72e2bd3ff3340e618fadacb3603838
Joseph Pentland <joe.pentland@gmail.com>
2017-12-04 22:46:35 +0100

    platform: nanopi: Add updated doc.go
    Refers to non-existent README.md (not forever)

---
+++ platforms/nanopi/doc.go
@@ -0,0 +1,7 @@
+/*
+Package nanopi contains the Gobot adaptor for the NanoPi.
+
+For further information refer to nanopi README:
+https://github.com/hybridgroup/gobot/blob/master/platforms/nanopi/README.md
+*/
+package nanopi // import "gobot.io/x/gobot/platforms/nanopi"
